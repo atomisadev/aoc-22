f4c1f81a4849e13687911483d58afc023dca9213
atomisadeveloper <atomdevelops@protonmail.com>
2022-12-02 21:02:08 +0000

    2022: day 1 part 2

--- Day 1/part2.ts
+++ Day 1/part2.ts
@@ -1,10 +1,12 @@
 import { input } from "./input";
+
 const sorted = input
-  .map((e) => {
-    return e
+  .map((elf) => {
+    return elf
       .split("\n")
       .map((i) => parseInt(i, 10))
       .reduce((sum, v) => sum + v, 0);
   })
-  .sort((x, y) => y - x);
-console.log(sorted[0]);
+  .sort((a, b) => b - a);
+
+console.log(sorted.slice(0, 3).reduce((sum, v) => sum + v, 0));
